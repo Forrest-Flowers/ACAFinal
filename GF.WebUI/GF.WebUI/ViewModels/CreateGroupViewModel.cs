4119ec4516db37266b2726c35f1d962dee94104b
unknown <forrest.w.flowers@gmail.com>
2019-04-15 14:30:26 -0500

    Working on Create Group page, ran into error. Ensuring all branches required are live.

---
+++ GF.WebUI/GF.WebUI/ViewModels/CreateGroupViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GF.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GF.WebUI.ViewModels
+{
+    public class CreateGroupViewModel
+    {
+        public Group Group { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
